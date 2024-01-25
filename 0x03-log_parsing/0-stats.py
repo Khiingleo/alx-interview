da7489c9c834157ebbb52e872211555a7284f05d
shalom ogoziem <shalomzymike@gmail.com>
2024-01-25 13:05:08 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
 """
-script that reads the stdin line by line and then computes the metrics
+reads stdin line by line and computes metrics
 """
 import sys
 
 
-line_counted = 0
+def print_stats(status_dict, total_size):
+    """prints the file size and stats for every 10 loops"""
+
+    print("File size: {}".format(total_size))
+    for key, val in sorted(status_dict.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_size = 0
-status_dict = {200: 0, 301: 0, 400: 0, 401: 0,
-               403: 0, 404: 0, 405: 0, 500: 0}
+status_code = 0
+line_count = 0
+status_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+
 
 try:
     for line in sys.stdin:
-        try:
-            line = line[:-1]
-            parts = line.split(' ')
-            total_size += int(parts[-1])
-            status_code = int(parts[-2])
-            if status_code in status_dict:
-                status_dict[status_code] += 1
-        except Exception:
-            pass
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-        if line_counted % 10 == 0:
-            print(f"File size: {total_size}")
-            for code in sorted(status_dict.keys()):
-                if status_dict[code] != 0:
-                    print(f"{code}: {status_dict[code]}")
+        if len(parsed_line) > 2:
+            line_count += 1
 
-except KeyboardInterrupt:
-    print(f"File size: {total_size}")
-    for code in sorted(status_dict.keys()):
-        if status_dict[code] != 0:
-            print(f"{code}: {status_dict[code]}")
+            if line_count <= 10:
+                total_size += int(parsed_line[0])
+                status_code = parsed_line[1]
 
-print(f"File size: {total_size}")
-for code in sorted(status_dict.keys()):
-    if status_dict[code] != 0:
-        print(f"{code}: {status_dict[code]}")
+                if status_code in status_dict.keys():
+                    status_dict[status_code] += 1
+
+            if line_count == 10:
+                print_stats(status_dict, total_size)
+                line_count = 0
+
+finally:
+    print_stats(status_dict, total_size)
